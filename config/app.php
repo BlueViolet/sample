ec0347fa0cefd7d3ea03466865cd4fa29917bdbb
王以晖 <465368912@qq.com>
2018-05-07 12:26:04 +0000

    Finish user signup

--- config/app.php
+++ config/app.php
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'zh-CN',
 
     /*
     |--------------------------------------------------------------------------
@@ -160,9 +160,11 @@ return [
         Illuminate\Redis\RedisServiceProvider::class,
         Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
         Illuminate\Session\SessionServiceProvider::class,
-        Illuminate\Translation\TranslationServiceProvider::class,
+        //Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        //第三方语言包
+        Overtrue\LaravelLang\TranslationServiceProvider::class,
 
         /*
          * Package Service Providers...
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','个人信息页面')
+@section('content')
+    <h1>个人信息页面显示</h1>
+@stop
